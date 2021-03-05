313dd9ae4c4114b98ce01ea79f208bddbd6b54a9
atsuko0914 <0914.atsuko@gmail.com>
2021-03-05 12:59:08 +0900

    googleでログイン

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,13 +3,14 @@
 @section('login')
 @section('title', 'ログイン')
 
-<a href="{{ route('login.{provider}', ['provider' => 'google']) }}" class="btn btn-block btn-danger">
-  <i class="fab fa-google mr-1"></i>Googleでログイン
-</a>
+
 
 @include('error_card_list')
 <div class="top_login">
   <h1>BookTreeにログイン</h1>
+  <a href="{{ route('login.{provider}', ['provider' => 'google']) }}" class="btn btn-block btn-danger">
+  <i class="fab fa-google mr-1"></i>Googleでログイン
+</a>
   <form action="{{ route('login') }}" method="POST">
   @csrf
 
--- resources/views/books/card.blade.php
+++ resources/views/books/card.blade.php
@@ -3,50 +3,51 @@
   
   @if( Auth::id() === $book->user_id )  
     <!-- dropdown -->
-      <div class="ml-auto card-text">
-        <div class="dropdown">
-          <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-ellipsis-v"></i>
+    <div class="ml-auto card-text">
+      <div class="dropdown">
+        <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-ellipsis-v"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right">
+          <a class="dropdown-item" href="{{ route("books.edit", ['book' => $book]) }}">
+            <i class="fas fa-pen mr-1"></i>本を更新する
           </a>
-          <div class="dropdown-menu dropdown-menu-right">
-            <a class="dropdown-item" href="{{ route("books.edit", ['book' => $book]) }}">
-              <i class="fas fa-pen mr-1"></i>本を更新する
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $book->id }}">
-              <i class="fas fa-trash-alt mr-1"></i>本を削除する
-            </a>
-          </div>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $book->id }}">
+            <i class="fas fa-trash-alt mr-1"></i>本を削除する
+          </a>
         </div>
       </div>
-      <!-- dropdown -->
+    </div>
+    <!-- dropdown -->
 
-      <!-- modal -->
-      <div id="modal-delete-{{ $book->id }}" class="modal fade" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <form method="POST" action="{{ route('books.destroy', ['book' => $book]) }}">
-              @csrf
-              @method('DELETE')
-              <div class="modal-body">
-                {{ $book->title }}を削除します。よろしいですか？
-              </div>
-              <div class="modal-footer justify-content-between">
-                <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
-                <button type="submit" class="btn btn-danger h-50">削除する</button>
-              </div>
-            </form>
+    <!-- modal -->
+    <div id="modal-delete-{{ $book->id }}" class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+              <span aria-hidden="true">&times;</span>
+            </button>
           </div>
+          <form method="POST" action="{{ route('books.destroy', ['book' => $book]) }}">
+            @csrf
+            @method('DELETE')
+            <div class="modal-body">
+              {{ $book->title }}を削除します。よろしいですか？
+            </div>
+            <div class="modal-footer justify-content-between">
+              <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
+              <button type="submit" class="btn btn-danger h-50">削除する</button>
+            </div>
+          </form>
         </div>
       </div>
-      <!-- modal -->
-      @endif
+    </div>
+    <!-- modal -->
+  @endif
   </div>
+
 
   @if($book->book_image_path)
     <img class="card-img-top" src="/storage/image/{{$book->book_image_path }}" alt="本の画像">
@@ -55,8 +56,8 @@
     <h3 class="card-title">
       {{ $book->title }}
     </h3>
-    <a class="text-dark" href="{{ route('books.show', ['book' => $book]) }}">
-    <small>詳細</small>
+    <a class="detail" href="{{ route('books.show', ['book' => $book]) }}">
+    詳細
     </a>
   </div>
 </div>
