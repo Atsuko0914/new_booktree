070d40d7e75a18d3f0d0b33e95c7421184baf350
atsuko0914 <atsuko@mbp.local>
2021-04-15 11:01:02 +0900

    API修正

--- app/Http/Controllers/Api/Bookcontroller.php
+++ app/Http/Controllers/Api/Bookcontroller.php
@@ -32,18 +32,45 @@ class Bookcontroller extends Controller
     }
 
     public function store(Request $request)
-    {
+    {   
         $book = new Book;
+        
+         if(empty($book->title = $request->title)) {
+
+                return $project =[
+                'status' => 300,
+                ];
+        } elseif (empty($book->user_id = $request->user_id)) {
+
+                return $project =[
+                'status' => 300,
+                ];
+
+        } elseif (empty($book->author = $request->author)) {
+
+                return $project =[
+                'status' => 300,
+                ];
+        } else {
+            
         $book->title = $request->title;
         $book->author = $request->author;
         $book->user_id = $request->user_id;
+        $book->purchase_date = $request->purchase_date;
+        $book->price = $request->price;
+        $book->publication = $request->publication;
+        $book->issue_date = $request->issue_date;
+        $book->keyword = $request->keyword;
+        $book->summary = $request->summary;
+        $book->user_id = $request->user_id;
         $book->save();
+        
+            return $project =[
+                'status'=>200,
+            ];
+        }
 
-        // $book = Book::get(['title', 'author', 'user_id']);
-
-        return $project =[
-            'status'=>200,
-        ];
+        
 
     }
 
@@ -51,7 +78,7 @@ class Bookcontroller extends Controller
     {
         $book = Book::find($id);
         $book->delete();
-        
+
         return $project =[
             'status'=>200,
         ];
